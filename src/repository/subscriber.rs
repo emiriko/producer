fbef8d77b1717c76807bb1bfc9624f42dbf37da5
emiriko <notalvaroaustin@gmail.com>
2024-06-01 13:01:39 +0700

    Implement update method in Subscriber model to send notification HTTP requests.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -1,7 +1,7 @@
 use dashmap::DashMap;
 use lazy_static::lazy_static;
 
-use crate::model::subscriber::{self, Subscriber};
+use crate::model::subscriber::Subscriber;
 
 // Singleton of Database
 lazy_static! {
